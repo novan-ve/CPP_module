a2e8985da637aa0b3df273874bd0b43ae17e8852
Noel Van veldhuisen <novan-ve@f0r4s9.codam.nl>
2020-10-11 18:00:10 +0200

    Finished m02

--- m02/ex01/main.cpp
+++ m02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: novan-ve <novan-ve@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/10 15:54:53 by novan-ve      #+#    #+#                 */
-/*   Updated: 2020/10/10 15:54:54 by novan-ve      ########   odam.nl         */
+/*   Updated: 2020/10/11 14:12:23 by novan-ve      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,23 @@
 #include <iostream>
 
 int main( void ) {
+
 	Fixed a;
 	Fixed const b( 10 );
 	Fixed const c( 42.42f );
 	Fixed const d( b );
+
 	a = Fixed( 1234.4321f );
+
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
 	return 0;
 }
--- m02/ex02/Fixed.cpp
+++ m02/ex02/Fixed.cpp
@@ -13,63 +13,55 @@
 #include "Fixed.hpp"
 #include <cmath>
 
-
 Fixed::Fixed( void ) : _val( 0 ) {
 
-	std::cout << "Default constructor called" << std::endl;
+//	std::cout << "Default constructor called" << std::endl;
 
 	return;
 }
 
-
 Fixed::Fixed( const int val ) {
 
-	std::cout << "Int constructor called" << std::endl;
+//	std::cout << "Int constructor called" << std::endl;
 
 	this->_val = val * ( 1 << this->_frBits );
 }
 
-
 Fixed::Fixed( const float val ) {
 
-	std::cout << "Float constructor called" << std::endl;
+//	std::cout << "Float constructor called" << std::endl;
 
 	this->_val = roundf( val * ( 1 << this->_frBits ) );
 }
 
-
 Fixed::Fixed( Fixed const & src ) {
 
-	std::cout << "Copy constructor called" << std::endl;
+//	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 
 	return;
 }
 
-
 Fixed::~Fixed( void ) {
 
-	std::cout << "Destructor called" << std::endl;
+//	std::cout << "Destructor called" << std::endl;
 
 	return;
 }
-
 
 int		Fixed::getRawBits( void ) const {
 
 	return this->_val;
 }
 
-
 void	Fixed::setRawBits( int const raw ) {
 
 	this->_val = raw;
 }
 
-
 Fixed &		Fixed::operator=( Fixed const & rhs ) {
 
-	std::cout << "Assignation operator called" << std::endl;
+//	std::cout << "Assignation operator called" << std::endl;
 
 	if ( this != &rhs )
 		this->_val = rhs.getRawBits();
@@ -77,18 +69,56 @@ Fixed &		Fixed::operator=( Fixed const & rhs ) {
 	return *this;
 }
 
+/* ************************************************************************************** */
+/*								Arithmetic operator overloads							  */
+/* ************************************************************************************** */
+
+Fixed		Fixed::operator+( Fixed const & rhs ) const {
+
+	return Fixed( this->toFloat() + rhs.toFloat() );
+}
+
+Fixed		Fixed::operator-( Fixed const & rhs ) const {
+
+	return Fixed( this->toFloat() - rhs.toFloat() );
+}
+
+Fixed		Fixed::operator*( Fixed const & rhs ) const {
+
+	return Fixed( this->toFloat() * rhs.toFloat() );
+}
+
+Fixed		Fixed::operator/( Fixed const & rhs ) const {
+
+	return Fixed( this->toFloat() / rhs.toFloat() );
+}
+
+/* ************************************************************************************** */
+/*							increment/decrement operator overloads						  */
+/* ************************************************************************************** */
+
+Fixed		Fixed::operator++( int ) {
+
+	const Fixed old( *this );
+	++( *this );
+	return old;
+}
+
+Fixed		Fixed::operator++( ) {
+
+	_val++;
+	return *this;
+}
 
 float 		Fixed::toFloat( void ) const {
 
 	return ( static_cast<float>(this->_val) / ( 1 << this->_frBits ) );
 }
 
-
 int 		Fixed::toInt( void ) const {
 
 	return ( this->_val / ( 1 << this->_frBits ) );
 }
-
 
 std::ostream &		operator<<( std::ostream & o, Fixed const & i ) {
 
@@ -96,6 +126,5 @@ std::ostream &		operator<<( std::ostream & o, Fixed const & i ) {
 
 	return o;
 }
-
 
 const int Fixed::_frBits = 8;
